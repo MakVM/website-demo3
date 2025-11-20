b4dd2fee90296325e04b2808c890c0c911d87997
MakVM <101976709+MakVM@users.noreply.github.com>
2025-11-20 15:57:41 +0300

    Update IntroSection.tsx

--- src/react-app/components/IntroSection.tsx
+++ src/react-app/components/IntroSection.tsx
@@ -30,6 +30,10 @@ export default function IntroSection({ threatName }: IntroSectionProps) {
           <div className="animate-pulse">SYSTEM MONITORING ACTIVE</div>
         </div>
       </div>
+
+      <div className="mt-12 text-green-500/60 font-mono text-xs">
+  <div className="animate-pulse">Powered by <a href="https://rapidapi.com/cpwatch/api/cpw-tracker" className="hover:text-green-400 transition-colors no-underline">CPW API</a></div>
+</div>
       
       <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
         <ChevronDown className="w-8 h-8 text-green-500/50" />
